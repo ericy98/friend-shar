82af6a22cd86542de679d212a4be969461ff0903
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-09-22 22:57:23 -0500

    user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: "Please enter a username",
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: "Please enter an email",
+            match: [/.+@.+\..+/, 'Must match an email address!'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User; 
